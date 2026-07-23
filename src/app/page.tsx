9ce5efc6202d759cc0095db37472251585f51f75
MohamedRamy-Elmagrabey <202401148@pua.edu.eg>
2026-07-23 21:40:44 +0300

    fix: hero text left-aligned on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           className="absolute right-0 z-[1] h-auto w-[350px] bottom-[40px] -translate-x-4 transition-all duration-500 ease-in-out sm:w-[450px] sm:bottom-[60px] sm:-translate-x-6 md:bottom-0 md:w-[560px] md:-translate-x-12 lg:bottom-0 lg:w-[750px] lg:-translate-x-20 xl:mt-[270px] xl:w-[1050px] xl:-translate-x-[120px] xl:bottom-auto"
           priority
         />
-        <div className="relative z-10 flex min-h-[90vh] items-center pl-4 pr-4 md:items-start md:pt-28 lg:items-start lg:pt-28 min-[1440px]:items-center min-[1440px]:pt-0">
+        <div className="relative z-10 flex min-h-[90vh] items-center text-left pl-4 pr-4 md:items-start md:pt-28 lg:items-start lg:pt-28 min-[1440px]:items-center min-[1440px]:pt-0">
           <div style={{ marginLeft: 30, marginTop: 60 }} className="max-w-[900px]">
             <h1 style={{ fontFamily: '"MedulaOne", serif' }} className="text-[58px] font-normal leading-[59px] text-white sm:text-[72px] sm:leading-[73px] md:text-[86px] md:leading-[87px] lg:text-[120px] lg:leading-[121px] xl:text-[132px] xl:leading-[133px] 2xl:text-[162px] 2xl:leading-[163px]">
               Where Ambition<br />Meets Engineering
